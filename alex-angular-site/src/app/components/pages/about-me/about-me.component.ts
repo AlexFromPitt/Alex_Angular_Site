c76482153df61a6b22e94de250ac10f7be55342c
AlexFromPitt <mitro.alex412@gmail.com>
2025-01-28 16:13:09 -0500

    Added Updates for About me Page.
    Updated About Me Page.
    Saving here before changing up the navigation

--- alex-angular-site/src/app/components/pages/about-me/about-me.component.ts
+++ alex-angular-site/src/app/components/pages/about-me/about-me.component.ts
@@ -8,11 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-  title: string = '';
-
+  title: string = 'About Me';
+  summary: string = "I\’m a software engineer from Pittsburgh, Pennsylvania, passionate about technology and continuous learning. A University of Pittsburgh graduate with a Bachelor’s in Computer Science, I bring over seven years of professional experience spanning the software development lifecycle. I’m seeking opportunities to contribute to meaningful projects that challenge me to grow and refine my skills. My philosophy is rooted in collaboration, innovation, and making a positive impact."
+  imagePath = 'assets/portrait_1.png';
+  
   constructor() {}
 
   ngOnInit() {
-    this.title = 'About Me';
   }
 }
